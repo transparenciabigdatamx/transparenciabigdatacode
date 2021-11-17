838f64a76a9e9e6526710c580c6d97b78f78e8f6
unknown <jesicatapia@gmail.com>
2021-11-17 00:49:48 -0600

    Deleted copy assets

--- dist/assets/scripts/nav.js
+++
@@ -1,8 +0,0 @@
-function cambiaClase() {
-  this.classList.toggle("is-active");
-  document.querySelector("body").classList.toggle("nav-activo");
-}
-
-document
-  .querySelector(".header-nav-icon")
-  .addEventListener("click", cambiaClase);
